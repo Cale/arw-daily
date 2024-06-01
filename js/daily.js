2c17ddf3c09d7a5b300c696d3de5c00cd17b98c0
Cale <cale@midnightcheese.com>
2024-05-31 21:38:07 -0500

    Add recent news page, javascript, dynamic date and time in header.

---
+++ js/daily.js
@@ -0,0 +1,57 @@
+t = setInterval(getTime,60000);
+
+function getTime() {
+
+    var isoString = new Date().toISOString()
+    var utc= (new Date(isoString)).toUTCString()
+
+    var day = new Date(utc).getUTCDay()
+    var month = new Date(utc).getUTCMonth()
+    var daynum = new Date(utc).getUTCDate()
+    var hour = new Date(utc).getUTCHours()
+    var minute = new Date(utc).getUTCMinutes()
+    var year = new Date(utc).getFullYear()
+
+    if (minute < 10) {
+        minute = "0"+minute
+    }
+
+    if (day == 0 ) {
+        day = "Sunday"
+    } else if (day == 1) {
+        day = "Monday"
+    } else if (day == 2) {
+        day = "Tuesday"
+    } else if (day == 3) {
+        day = "Wednesday"
+    } else if (day == 4) {
+        day = "Thursday"
+    } else if (day == 5) {
+        day = "friday"
+    } else if (day == 6) {
+        day = "Saturday"
+    }
+
+    if (month == 0 ) {
+        month = "January"
+    } else if (month == 1) {
+        month = "February"
+    } else if (month == 2) {
+        month = "March"
+    } else if (month == 3) {
+        month = "April"
+    } else if (month == 4) {
+        month = "May"
+    } else if (month == 5) {
+        month = "June"
+    } else if (month == 6) {
+        month = "July"
+    }
+
+    var headerdate = day + " " + month + " " + daynum + ", " + year + " " + hour + ":" + minute + " UTC"
+
+    document.getElementById("header-date").innerHTML = ""
+    document.getElementById("header-date").innerHTML = headerdate
+}
+
+getTime()
